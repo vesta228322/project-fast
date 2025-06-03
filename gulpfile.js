2966ca68bfdb1e51b257aa67b9b7f494ffe7e4f9
vesta <hotabych39@gmail.com>
2025-06-03 19:33:31 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+
+// Static server
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+});
+
+gulp.task('styles', function() {
+    return gulp.src("")
+})
